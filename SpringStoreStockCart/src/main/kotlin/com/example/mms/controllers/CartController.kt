5945c3ffc686cfb2ce882665655db5088d1e1882
MathieuMarthy <mathieu.marthy@gmail.com>
2023-12-07 16:09:12 +0100

    path variable

--- SpringStoreStockCart/src/main/kotlin/com/example/mms/controllers/CartController.kt
+++ SpringStoreStockCart/src/main/kotlin/com/example/mms/controllers/CartController.kt
@@ -5,45 +5,42 @@ import com.example.mms.dto.asCartDTO
 import com.example.mms.errors.CartAlreadyExistsException
 import com.example.mms.errors.CartNotFoundException
 import com.example.mms.repository.CartRepository
+import jakarta.validation.Valid
 import jakarta.validation.constraints.Min
-import jakarta.websocket.server.PathParam
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder
+import java.net.URI
 
 @RestController
 @Validated
 class CartController(val cartRepository: CartRepository) {
 
-    @PostMapping("api/cart")
-    fun create(@RequestBody userId: Int): ResponseEntity<CartDTO> {
-        println("aaaaaaaaaaaaaaaa")
-
+    @PostMapping("api/cart/{userId}")
+    fun create(@PathVariable userId: Int): ResponseEntity<CartDTO> {
         val resCart = this.cartRepository.create(userId)
 
         if (resCart.isFailure) {
             throw CartAlreadyExistsException(userId)
         }
 
-        return ResponseEntity.ok(resCart.getOrNull()!!.asCartDTO())
+        val location: URI = ServletUriComponentsBuilder
+            .fromCurrentRequest()
+            .buildAndExpand()
+            .toUri()
+        return ResponseEntity.created(location).body(resCart.getOrNull()!!.asCartDTO())
     }
 
-    @GetMapping("/api/cart/{userId}")
+    @GetMapping("api/cart/{userId}")
     fun get(@PathVariable userId: Int): ResponseEntity<CartDTO> {
-        println("aaaaaaaaaaaaaaaa")
         val cart = this.cartRepository.get(userId) ?: throw CartNotFoundException(userId)
 
         return ResponseEntity.ok(cart.asCartDTO())
     }
 
-    @DeleteMapping("api/cart")
-    fun delete(@RequestBody userId: Int): ResponseEntity<CartDTO> {
+    @DeleteMapping("api/cart/{userId}")
+    fun delete(@PathVariable userId: Int): ResponseEntity<CartDTO> {
         val resCart = this.cartRepository.delete(userId)
 
         if (resCart.isFailure) {
@@ -53,8 +50,12 @@ class CartController(val cartRepository: CartRepository) {
         return ResponseEntity.ok(resCart.getOrNull()!!.asCartDTO())
     }
 
-    @PutMapping("api/cart/item")
-    fun updateItem(@RequestBody userId: Int, itemId: Int, @Min(0) quantity: Int): ResponseEntity<CartDTO> {
+    @PutMapping("api/cart/{userId}/item")
+    fun updateItem(
+        @PathVariable userId: Int,
+        @RequestBody itemId: Int,
+        @RequestBody @Valid @Min(0) quantity: Int
+    ): ResponseEntity<CartDTO> {
         // TODO: vérifier le stock dans l'api des items
         val resCart = this.cartRepository.updateItem(userId, itemId, quantity)
 
@@ -65,8 +66,8 @@ class CartController(val cartRepository: CartRepository) {
         return ResponseEntity.ok(resCart.getOrNull()!!.asCartDTO())
     }
 
-    @DeleteMapping("api/cart/item")
-    fun deleteItem(@RequestBody userId: Int, itemId: Int): ResponseEntity<CartDTO> {
+    @DeleteMapping("api/cart/{userId}/item")
+    fun deleteItem(@PathVariable userId: Int, @RequestBody itemId: Int): ResponseEntity<CartDTO> {
         val resCart = this.cartRepository.deleteItem(userId, itemId)
 
         if (resCart.isFailure) {
@@ -76,8 +77,8 @@ class CartController(val cartRepository: CartRepository) {
         return ResponseEntity.ok(resCart.getOrNull()!!.asCartDTO())
     }
 
-    @PostMapping("api/cart/valid")
-    fun valid(@RequestBody userId: Int): ResponseEntity.BodyBuilder {
+    @PostMapping("api/cart/{userId}/valid")
+    fun valid(@PathVariable userId: Int): ResponseEntity.BodyBuilder {
         // TODO: vérifier le stock dans l'api des items
         this.cartRepository.valid(userId)
 
--- SpringStoreStockCart/src/main/kotlin/com/example/mms/models/Cart.kt
+++ SpringStoreStockCart/src/main/kotlin/com/example/mms/models/Cart.kt
@@ -2,14 +2,13 @@ package com.example.mms.models
 
 import jakarta.persistence.*
 
-
 @Entity
 @Table(name = "carts")
 data class Cart(
-        @Id
-        val id: Int,
+    @Id
+    val id: Int,
 
-        @OneToMany(cascade = [CascadeType.ALL])
-        @JoinColumn(referencedColumnName = "id")
-        val items: MutableList<ItemInCart> = mutableListOf()
+    @OneToMany(cascade = [CascadeType.ALL])
+    @JoinColumn(referencedColumnName = "id")
+    val items: MutableList<ItemInCart> = mutableListOf()
 )
