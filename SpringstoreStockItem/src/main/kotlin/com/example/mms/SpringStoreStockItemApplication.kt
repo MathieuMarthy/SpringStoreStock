4eb4c6506b58335ffbc1d0216ccdb7686d943135
Sylvain Baudouin <sylvain.baudouin@etu.univ-nantes.fr>
2023-12-07 11:43:16 +0100

    Item repo

---
+++ SpringstoreStockItem/src/main/kotlin/com/example/mms/SpringStoreStockItemApplication.kt
@@ -0,0 +1,11 @@
+package com.example.mms
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringStoreStockItemApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringStoreStockItemApplication>(*args)
+}
---
+++ SpringstoreStockItem/src/test/kotlin/com/example/mms/SpringStoreStockItemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.mms
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringStoreStockItemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
