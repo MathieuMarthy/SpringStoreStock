9b76d5a3fc54a0377c9fb56fb36263a1f33aaa0e
E214977W <mathieu.marthy@etu.univ-nantes.fr>
2023-12-07 11:20:24 +0100

    repo in memory + controller

--- SpringStoreStockCart/src/main/kotlin/com/example/mms/repository/CartInMemoryRepository.kt
+++ SpringStoreStockCart/src/main/kotlin/com/example/mms/repository/CartInMemoryRepository.kt
@@ -1,26 +1,84 @@
 package com.example.mms.repository
 
-import com.example.mms.Models.Cart
+import com.example.mms.errors.CartAlreadyExistsException
+import com.example.mms.errors.CartNotFoundException
+import com.example.mms.models.Cart
+import com.example.mms.models.ItemInCart
+import org.springframework.stereotype.Service
 
-class CartInMemoryRepository: CartRepository {
+@Service
+class CartInMemoryRepository : CartRepository {
 
-    private val carts = mutableMapOf<Int,Cart>()
-    override fun get(id : Int) = carts[id]
-    override fun create(cart: Cart) : Result<Cart> {
-        val prev = carts.putIfAbsent(cart.id, cart)
+    private val carts = mutableMapOf<Int, Cart>()
+
+    override fun get(id: Int) = carts[id]
+
+    override fun create(id: Int): Result<Cart> {
+        val cart = Cart(id)
+
+        val prev = carts.putIfAbsent(id, cart)
+
         return if (prev == null) {
             Result.success(cart)
         } else {
-            Result.failure(Exception("Cart already exists"))
+            Result.failure(CartAlreadyExistsException("Cart already exists"))
         }
     }
-    override fun update(cart: Cart) : Result<Cart> {
-        val updated = carts.replace(cart.id, cart)
-        return if (updated == null) {
-            Result.failure(Exception("Cart doesn't exist"))
+
+    override fun updateItem(id: Int, itemId: Int, quantity: Int): Result<Cart> {
+        val cart = this.get(id) ?: return Result.failure(CartNotFoundException(id))
+
+        // Check if item is already in the items list
+        val item = cart.items.find { i -> i.itemId == itemId }
+        if (item != null) {
+            // set the quantity
+            item.quantity = quantity
         } else {
-            Result.success(cart)
+            // add a new ItemInCart into items list
+            cart.items.add(
+                ItemInCart(
+                    this.getNextItemInCartId(cart),
+                    itemId,
+                    quantity
+                )
+            )
         }
+
+        // TODO: regarde si ca update bien, si le modifier cart change aussi cart dans la liste
+        return Result.success(cart)
     }
-    override fun delete(cart: Cart) = carts.remove(cart.id)
+
+
+    override fun update(newCart: Cart): Result<Cart> {
+        if (this.exists(newCart.id)) {
+            return Result.failure(CartNotFoundException(newCart.id))
+        }
+
+        this.carts.replace(newCart.id, newCart)
+        return Result.success(this.get(newCart.id)!!)
+    }
+
+    override fun deleteItem(id: Int, itemId: Int): Result<Cart> {
+        val cart = this.get(id) ?: return Result.failure(CartNotFoundException(id))
+
+        cart.items.removeIf { e -> e.itemId == itemId }
+        return Result.success(cart)
+    }
+
+    override fun delete(id: Int): Result<Cart> {
+        val cart = this.carts.remove(id) ?: return Result.failure(CartNotFoundException(id))
+
+        return Result.success(cart)
+    }
+
+    override fun valid(id: Int): Boolean {
+        val cart = this.get(id) ?: return false
+
+        cart.items.clear()
+        return true
+    }
+
+    private fun getNextItemInCartId(cart: Cart): Int = cart.items.size + 1
+
+    private fun exists(id: Int) = this.carts.containsKey(id)
 }
--- SpringStoreStockCart/src/main/kotlin/com/example/mms/repository/CartRepository.kt
+++ SpringStoreStockCart/src/main/kotlin/com/example/mms/repository/CartRepository.kt
@@ -1,10 +1,13 @@
 package com.example.mms.repository
 
-import com.example.mms.Models.Cart
+import com.example.mms.models.Cart
 
 interface CartRepository {
-    fun get(id : Int): Cart?
-    fun create(cart: Cart) : Result<Cart>
-    fun update(cart: Cart) : Result<Cart>
-    fun delete(cart: Cart) : Cart?
+    fun get(id: Int): Cart?
+    fun create(id: Int): Result<Cart>
+    fun updateItem(id: Int, itemId: Int, quantity: Int): Result<Cart>
+    fun deleteItem(id: Int, itemId: Int): Result<Cart>
+    fun update(newCart: Cart): Result<Cart>
+    fun delete(id: Int): Result<Cart>
+    fun valid(id: Int): Boolean
 }
