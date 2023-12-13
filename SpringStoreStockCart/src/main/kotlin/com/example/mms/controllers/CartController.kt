6f996e8e565d5f71db6679260a011a9aba655fbb
E214977W <mathieu.marthy@etu.univ-nantes.fr>
2023-12-13 11:47:04 +0100

    swagger

--- SpringStoreStockCart/src/main/kotlin/com/example/mms/controllers/CartController.kt
+++ SpringStoreStockCart/src/main/kotlin/com/example/mms/controllers/CartController.kt
@@ -10,12 +10,18 @@ import com.example.mms.errors.UserNotFoundException
 import com.example.mms.repository.CartRepository
 import com.example.mms.services.ItemService
 import com.example.mms.services.UserService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder
 import java.net.URI
+
 
 @RestController
 @Validated
@@ -25,6 +31,15 @@ class CartController(
     private val userService: UserService
 ) {
 
+    @Operation(summary = "Create a cart for a user", description = "Create a cart for a user, the user must exist")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "201", description = "Cart created",
+                content = [Content(mediaType = "application/json", schema = Schema(implementation = CartDTO::class))]),
+        ApiResponse(responseCode = "404", description = "User not found",
+                content = [Content(mediaType = "application/json")]),
+        ApiResponse(responseCode = "409", description = "Cart already exists",
+                content = [Content(mediaType = "application/json")])
+    ])
     @PostMapping("api/cart/{userId}")
     fun create(@PathVariable userId: String): ResponseEntity<CartDTO> {
         if (!this.userService.userExists(userId)) {
@@ -44,6 +59,13 @@ class CartController(
         return ResponseEntity.created(location).body(resCart.getOrNull()!!.asCartDTO())
     }
 
+    @Operation(summary = "Get a cart for a user", description = "Get a cart for a user")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "User's cart",
+                content = [Content(mediaType = "application/json", schema = Schema(implementation = CartDTO::class))]),
+        ApiResponse(responseCode = "404", description = "Cart not found",
+                content = [Content(mediaType = "application/json")])
+    ])
     @GetMapping("api/cart/{userId}")
     fun get(@PathVariable userId: String): ResponseEntity<CartDTO> {
         val cart = this.cartRepository.get(userId) ?: throw CartNotFoundException(userId)
@@ -51,6 +73,13 @@ class CartController(
         return ResponseEntity.ok(cart.asCartDTO())
     }
 
+    @Operation(summary = "Delete a cart for a user", description = "Delete a cart for a user")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "the deleted cart",
+                content = [Content(mediaType = "application/json", schema = Schema(implementation = CartDTO::class))]),
+        ApiResponse(responseCode = "404", description = "Cart not found",
+                content = [Content(mediaType = "application/json")])
+    ])
     @DeleteMapping("api/cart/{userId}")
     fun delete(@PathVariable userId: String): ResponseEntity<CartDTO> {
         val resCart = this.cartRepository.delete(userId)
@@ -62,6 +91,15 @@ class CartController(
         return ResponseEntity.ok(resCart.getOrNull()!!.asCartDTO())
     }
 
+    @Operation(summary = "Add an item to a cart", description = "Add an item to a cart, the item must exist and the quantity must be available")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "the updated cart",
+                content = [Content(mediaType = "application/json", schema = Schema(implementation = CartDTO::class))]),
+        ApiResponse(responseCode = "404", description = "Cart not found",
+                content = [Content(mediaType = "application/json")]),
+        ApiResponse(responseCode = "409", description = "Item not enough stock",
+                content = [Content(mediaType = "application/json")])
+    ])
     @PostMapping("api/cart/{userId}/item")
     fun addItem(
         @PathVariable userId: String,
@@ -83,6 +121,17 @@ class CartController(
         return ResponseEntity.ok(resCart.getOrNull()!!.asCartDTO())
     }
 
+    @Operation(summary = "Update an item in a cart", description = "Update an item in a cart, the item must exist and the quantity must be available")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "the updated cart",
+                content = [Content(mediaType = "application/json", schema = Schema(implementation = CartDTO::class))]),
+        ApiResponse(responseCode = "404", description = "Cart not found",
+                content = [Content(mediaType = "application/json")]),
+        ApiResponse(responseCode = "404", description = "Item not Found",
+                content = [Content(mediaType = "application/json")]),
+        ApiResponse(responseCode = "409", description = "Item not enough stock",
+                content = [Content(mediaType = "application/json")])
+    ])
     @PutMapping("api/cart/{userId}/item")
     fun updateItem(
         @PathVariable userId: String,
@@ -104,6 +153,15 @@ class CartController(
         return ResponseEntity.ok(resCart.getOrNull()!!.asCartDTO())
     }
 
+    @Operation(summary = "Delete an item in a cart", description = "Delete an item in a cart")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "the deleted cart",
+                content = [Content(mediaType = "application/json", schema = Schema(implementation = CartDTO::class))]),
+        ApiResponse(responseCode = "404", description = "Cart not found",
+                content = [Content(mediaType = "application/json")]),
+        ApiResponse(responseCode = "404", description = "Item not Found",
+                content = [Content(mediaType = "application/json")])
+    ])
     @DeleteMapping("api/cart/{userId}/item")
     fun deleteItem(@PathVariable userId: String, @RequestBody itemId: Int): ResponseEntity<CartDTO> {
         val resCart = this.cartRepository.deleteItem(userId, itemId)
@@ -115,6 +173,15 @@ class CartController(
         return ResponseEntity.ok(resCart.getOrNull()!!.asCartDTO())
     }
 
+    @Operation(summary = "Valid a cart", description = "Valid a cart, the cart must not be empty and all items must be valid")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "the valid cart",
+                content = [Content(mediaType = "application/json", schema = Schema(implementation = CartDTO::class))]),
+        ApiResponse(responseCode = "404", description = "Cart not found",
+                content = [Content(mediaType = "application/json")]),
+        ApiResponse(responseCode = "404", description = "Item not Found",
+                content = [Content(mediaType = "application/json")])
+    ])
     @PostMapping("api/cart/{userId}/valid")
     fun valid(@PathVariable userId: String): ResponseEntity<String> {
         val cart = this.cartRepository.get(userId) ?: throw CartNotFoundException(userId)
