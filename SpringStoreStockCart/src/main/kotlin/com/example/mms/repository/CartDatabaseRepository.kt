adbce9f60fca54b41d3d660996a112aa1d0ab858
Sylvain Baudouin <sylvain.baudouin@etu.univ-nantes.fr>
2023-12-13 17:18:18 +0100

    Loggers

--- SpringStoreStockCart/src/main/kotlin/com/example/mms/repository/CartDatabaseRepository.kt
+++ SpringStoreStockCart/src/main/kotlin/com/example/mms/repository/CartDatabaseRepository.kt
@@ -6,21 +6,26 @@ import com.example.mms.errors.ItemAlreadyExistsException
 import com.example.mms.errors.ItemNotFoundException
 import com.example.mms.models.Cart
 import com.example.mms.models.ItemInCart
+import org.slf4j.LoggerFactory
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Repository
 
 @Repository
 class CartDatabaseRepository(private val jpa: CartJpaRepository) : CartRepository {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
     override fun get(id: String): Cart {
         return jpa.findByIdOrNull(id) ?: throw CartNotFoundException(id)
     }
 
     override fun create(id: String): Cart {
         if (this.jpa.existsById(id)) {
+            logger.error("Cart $id already exists")
             throw CartAlreadyExistsException(id)
         }
         val cart = Cart(id)
+        logger.info("Creating cart $id")
         return this.jpa.save(cart)
     }
 
@@ -31,6 +36,7 @@ class CartDatabaseRepository(private val jpa: CartJpaRepository) : CartRepositor
         val item = cart.items.find { i -> i.itemId == itemId }
 
         if (item != null) {
+            logger.error("Item $itemId already in cart $id")
             throw ItemAlreadyExistsException(id)
         }
 
@@ -40,36 +46,46 @@ class CartDatabaseRepository(private val jpa: CartJpaRepository) : CartRepositor
                         quantity
                 )
         )
-
+        logger.info("Adding item $itemId to cart $id")
         return this.jpa.save(cart)
     }
 
     override fun updateItem(id: String, itemId: Int, quantity: Int): Cart {
         val cart = this.get(id)
 
-        val item = cart.items.find { i -> i.itemId == itemId } ?: throw ItemNotFoundException(id)
+        val item = cart.items.find { i -> i.itemId == itemId }
+        if (item == null) {
+            logger.error("Item $itemId not in cart $id")
+            throw ItemNotFoundException(id)
+        }
         item.quantity = quantity
-
+        logger.info("Updating item $itemId in cart $id")
         return this.jpa.save(cart)
     }
 
     override fun deleteItem(id: String, itemId: Int): Cart {
         val cart = this.get(id)
 
-        val item = cart.items.find { i -> i.itemId == itemId } ?: throw ItemNotFoundException(id)
+        val item = cart.items.find { i -> i.itemId == itemId }
+        if (item == null) {
+            logger.error("Item $itemId not in cart $id")
+            throw ItemNotFoundException(id)
+        }
         cart.items.remove(item)
-
+        logger.info("Deleting item $itemId in cart $id")
         return this.jpa.save(cart)
     }
 
     override fun update(newCart: Cart): Cart {
         this.get(newCart.id)
+        logger.info("Updating cart ${newCart.id}")
         return this.jpa.save(newCart)
     }
 
     override fun delete(id: String): Cart {
         val cart = this.get(id)
         this.jpa.delete(cart)
+        logger.info("Deleting cart $id")
         return cart
     }
 
@@ -78,6 +94,7 @@ class CartDatabaseRepository(private val jpa: CartJpaRepository) : CartRepositor
 
         cart.items.clear()
         this.jpa.save(cart)
+        logger.info("Validating cart $id")
         return true
     }
 }
--- SpringStoreStockUser/src/main/kotlin/com/example/mms/Repository/UserDatabaseRepository.kt
+++ SpringStoreStockUser/src/main/kotlin/com/example/mms/Repository/UserDatabaseRepository.kt
@@ -2,19 +2,25 @@ package com.example.mms.Repository
 
 import com.example.mms.Errors.UserAlreadyExistsException
 import com.example.mms.Model.User
+import org.slf4j.LoggerFactory
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import org.springframework.stereotype.Service
 import java.util.*
+import java.util.logging.Logger
 import kotlin.jvm.optionals.getOrNull
 
 @Repository
 class UserDatabaseRepository(private val jpa : UserJpaRepository) : UserRepository {
 
+    private var logger = LoggerFactory.getLogger(javaClass)
+
     override fun create(user: User): Result<User> = if (jpa.findById(user.email).isPresent) {
+        logger.error("User ${user.email} already in DB")
         Result.failure(UserAlreadyExistsException(user.email))
     } else {
         val saved = jpa.save(user.asEntity())
+        logger.info("Creating user ${user.email}")
         Result.success(saved.asUser())
     }
 
@@ -28,8 +34,10 @@ class UserDatabaseRepository(private val jpa : UserJpaRepository) : UserReposito
 
     override fun update(user: User): Result<User> = if (jpa.findById(user.email).isPresent) {
         val saved = jpa.save(user.asEntity())
+        logger.info("Updating user ${user.email}")
         Result.success(saved.asUser())
     } else {
+        logger.error("User ${user.email} not found")
         Result.failure(Exception("User not in DB"))
     }
 
@@ -44,8 +52,10 @@ class UserDatabaseRepository(private val jpa : UserJpaRepository) : UserReposito
         if (user.isPresent) {
             user.get().lastCommand = Date()
             val saved = jpa.save(user.get())
+            logger.info("Updating last command date for user ${user.get().email}")
             return Result.success(saved.asUser())
         }
+        logger.error("User $email not found")
         return Result.failure(Exception("User not in DB"))
     }
 }
