78ab4fda75c8a8bc8cbfee7d86ba39b0ab4f0f6c
Sylvain Baudouin <sylvain.baudouin273@gmail.com>
2023-12-13 19:56:01 +0100

    UserUpdateDTO + tags swagger

---
+++ SpringStoreStockUser/src/main/kotlin/com/example/mms/Controller/DTO/UserDTO.kt
@@ -0,0 +1,17 @@
+package com.example.mms.Controller.DTO
+
+import com.example.mms.Model.User
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.Size
+import jdk.jfr.BooleanFlag
+
+
+data class UserDTO(
+    @field:Email val email: String,
+    @field:Size(min = 1, max = 30) val firstName: String,
+    @field:Size(min = 1, max = 30) val lastName: String,
+    @field:Size(min = 1, max = 30) val address: String,
+    @field:BooleanFlag val followingNewsletter: Boolean
+    ){
+    fun asUser() = User(email, firstName, lastName, address,followingNewsletter,null)
+}
